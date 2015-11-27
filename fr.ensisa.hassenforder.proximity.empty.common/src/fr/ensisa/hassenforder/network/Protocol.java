a44a6c9213d713ee32fc8a265e08f2ec7f93e2bb
Cédric Szymanski <szymanski.cedric@gmail.com>
2015-11-27 23:56:57 +0100

    Adding Protocol Connect Query into Protocol class

--- fr.ensisa.hassenforder.proximity.empty.common/src/fr/ensisa/hassenforder/network/Protocol.java
+++ fr.ensisa.hassenforder.proximity.empty.common/src/fr/ensisa/hassenforder/network/Protocol.java
@@ -4,6 +4,8 @@ public interface Protocol {
 
 	public static final Object EXIT_TEXT = "exit";
 
-	public static final int PROXIMITY_PORT_ID = 9876;
+	public static final int PROXIMITY_PORT_ID = 1337;
+	
+	public static final int CONNECT_QUERY = 1;
 
 }
